15301cdd6afc0d9f0d33e4e5f0d08a47e66be3dd
Athenea Sánchez Torres <sancheztorresathenea@gmail.com>
2025-04-02 11:09:56 -0600

    Update main.c

--- Generica_bicola/src/main.c
+++ Generica_bicola/src/main.c
@@ -1,43 +1,37 @@
 #define LINKED_LIST_IMPLEMENTATION
-#include "include.h"
-#include <stdio.h>
+#include "../include/include.h" 
 
 void print_int(int value) {
     printf("%d", value);
 }
 
-bool int_cmp(int a, int b) {
-    return a == b;
-}
-
-void print_char(char value) {
-    printf("%c", value);
-}
-
-bool char_cmp(char a, char b) {
-    return a == b;
-}
-
 int main() {
-    List_int* nums = list_int_create();
-    list_int_append(nums, 10);
-    list_int_append(nums, 20);
-    
-    printf("Lista de enteros: ");
-    list_int_print(nums, print_int); // Usar función concreta para evitar errores
-    
-    list_int_destroy(nums);
-    
-    List_char* chars = list_char_create();
-    list_char_append(chars, 'a');
-    list_char_append(chars, 'b');
-    list_char_append(chars, 'c');
-    list_char_append(chars, 'd');
-    list_char_append(chars, 'e');
+    Deque_int* deque = deque_int_create();
+    if (!deque) {
+        printf("Error al crear la deque.\n");
+        return 1;
+    }
 
-    printf("Lista de caracteres: ");
-    list_char_print(chars, print_char);
+    deque_int_push_back(deque, 10);
+    deque_int_push_back(deque, 20);
+    deque_int_push_front(deque, 5);
+    deque_int_push_front(deque, 1);
 
-    list_char_destroy(chars);
+    printf("Contenido de la deque: ");
+    deque_int_print(deque, print_int);
+
+    int value;
+    if (deque_int_pop_front(deque, &value)) {
+        printf("Elemento eliminado del frente: %d\n", value);
+    }
+
+    if (deque_int_pop_back(deque, &value)) {
+        printf("Elemento eliminado del final: %d\n", value);
+    }
+
+    printf("Contenido de la deque después de las eliminaciones: ");
+    deque_int_print(deque, print_int);
+
+    deque_int_destroy(deque);
     return 0;
 }
