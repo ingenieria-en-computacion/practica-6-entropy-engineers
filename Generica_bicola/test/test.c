21afa1eb927b4b094a2b8a27aaa4eae44562fa66
Athenea Sánchez Torres <sancheztorresathenea@gmail.com>
2025-04-02 11:10:23 -0600

    Update test.c

--- Generica_bicola/test/test.c
+++ Generica_bicola/test/test.c
@@ -1,102 +1,61 @@
 #include <check.h>
-#include <stdlib.h>
+
 #define LINKED_LIST_IMPLEMENTATION
-#include "linkedlist.h"
+#include "include.h" 
+START_TEST(test_deque_int_push_pop) {
+    Deque_int* deque = deque_int_create();
+    ck_assert_ptr_ne(deque, NULL);
 
-// Función auxiliar para imprimir enteros (usada en pruebas)
-void print_int(int value) {
-    printf("%d ", value);
-}
+    ck_assert(deque_int_push_back(deque, 10));
+    ck_assert(deque_int_push_back(deque, 20));
+    ck_assert(deque_int_push_front(deque, 5));
+    ck_assert(deque_int_push_front(deque, 1));
 
-// Función auxiliar para comparar enteros
-bool int_cmp(int a, int b) {
-    return a == b;
-}
+    int value;
+    ck_assert(deque_int_pop_front(deque, &value));
+    ck_assert_int_eq(value, 1);
 
-/* ------------------------------------- */
-/* Tests para listas de enteros (List_int) */
-/* ------------------------------------- */
-
-START_TEST(test_list_create) {
-    List_int* list = list_int_create();
-    ck_assert_ptr_nonnull(list);
-    ck_assert_ptr_null(list->head);
-    ck_assert_ptr_null(list->tail);
-    ck_assert_uint_eq(list->length, 0);
-    list_int_destroy(list);
-}
-END_TEST
-
-START_TEST(test_append_and_length) {
-    List_int* list = list_int_create();
-    
-    ck_assert(list_int_append(list, 10));
-    ck_assert_uint_eq(list_int_length(list), 1);
-    
-    ck_assert(list_int_append(list, 20));
-    ck_assert_uint_eq(list_int_length(list), 2);
-    
-    list_int_destroy(list);
-    ck_assert_ptr_null(list);
-}
-END_TEST
-
-START_TEST(test_insert_and_get) {
-    List_int* list = list_int_create();
-    int value = 0;
-    
-    list_int_insert(list, 10, 0);
-    list_int_insert(list, 30, 1);
-    list_int_insert(list, 20, 1);    
-    
-    ck_assert(list_int_get(list, 1, &value));
+    ck_assert(deque_int_pop_back(deque, &value));
     ck_assert_int_eq(value, 20);
-    
-    list_int_destroy(list);
+
+    ck_assert(deque_int_pop_front(deque, &value));
+    ck_assert_int_eq(value, 5);
+
+    ck_assert(deque_int_pop_back(deque, &value));
+    ck_assert_int_eq(value, 10);
+
+    ck_assert(deque_int_is_empty(deque));
+
+    deque_int_destroy(deque);
 }
 END_TEST
 
-START_TEST(test_remove) {
-    List_int* list = list_int_create();
-    list_int_append(list, 10);
-    list_int_append(list, 20);
-    list_int_append(list, 30);
-    
-    ck_assert(list_int_remove_at(list, 1));  // Elimina 20
-    ck_assert_uint_eq(list_int_length(list), 2);
-    
-    int value = 0;
-    ck_assert(list_int_get(list, 1, &value));
-    ck_assert_int_eq(value, 30);  // Lista: [10, 30]
-    
-    list_int_destroy(list);
-}
-END_TEST
+Suite* deque_suite(void) {
+    Suite* s;
+    TCase* tc_core;
 
-/* ------------------------------------- */
-/* Suite de pruebas principal */
-/* ------------------------------------- */
+    s = suite_create("Deque");
 
-Suite* linked_list_suite(void) {
-    Suite* s = suite_create("Linked List");
-    
-    TCase* tc_core = tcase_create("Core Functions");
-    tcase_add_test(tc_core, test_list_create);
-    tcase_add_test(tc_core, test_append_and_length);
-    tcase_add_test(tc_core, test_insert_and_get);
-    tcase_add_test(tc_core, test_remove);
-    
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_deque_int_push_pop);
     suite_add_tcase(s, tc_core);
+
     return s;
 }
 
 int main(void) {
     int number_failed;
-    SRunner* sr = srunner_create(linked_list_suite());
-    
+    Suite* s;
+    SRunner* sr;
+
+    s = deque_suite();
+    sr = srunner_create(s);
+
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
-    
+
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
